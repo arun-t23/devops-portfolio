0ec926ed25c3f09dd58062018c625b7888fe04b2
Arun T <arunt98>
2025-07-13 13:01:07 -0400

    changes in github

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,6 +5,7 @@ const Contact = () => (
       <h2 className="text-3xl font-bold mb-4">Contact</h2>
       <p className="mb-4">Email: <a href="mailto:arunkumart98@outlook.com" className="text-blue-400 hover:underline">arunkumart98@outlook.com</a></p>
       <p className="mb-6">LinkedIn: <a href="https://linkedin.com/in/arunkumar1866" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">linkedin.com/in/arunkumar1866</a></p>
+      <p className="mb-6">GitHub: <a href="https://github.com/arun-t23" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">github.com/arun-t23</a></p>
 
     </div>
   </section>
